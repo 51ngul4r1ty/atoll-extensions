ab47e8e7a682a388e9e24e879dd672f51cddcafa
Kevin Berry <41717340+51ngul4r1ty@users.noreply.github.com>
2022-04-28 21:15:52 -0400

    initial commit

---
+++ dist/extension.js
@@ -0,0 +1,103 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __webpack_require__(1);
+let myStatusBarItem;
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "atoll-extension" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand("atoll-extension.helloWorld", () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage("Hello World from Atoll Extension!");
+    });
+    context.subscriptions.push(disposable);
+    const myCommandId = "atoll-extension.showSelectionCount";
+    let disposable2 = vscode.commands.registerCommand(myCommandId, () => {
+        // const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+        // vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
+        vscode.window.showInformationMessage("gh-401: As a developer, I can use Atoll within VS Code, so that I can be more productive");
+    });
+    context.subscriptions.push(disposable2);
+    myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10000);
+    myStatusBarItem.command = myCommandId;
+    context.subscriptions.push(myStatusBarItem);
+    // register some listener that make sure the status bar
+    // item always up-to-date
+    // context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+    // context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+    // update status bar item once at start
+    updateStatusBarItem();
+}
+exports.activate = activate;
+function getNumberOfSelectedLines(editor) {
+    let lines = 0;
+    if (editor) {
+        lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
+    }
+    return lines;
+}
+function updateStatusBarItem() {
+    // https://code.visualstudio.com/api/references/icons-in-labels
+    myStatusBarItem.text = `$(extensions-star-full) gh-401 » basic extension UI`;
+    myStatusBarItem.show();
+}
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
---
+++ src/extension.ts
@@ -0,0 +1,64 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+let myStatusBarItem: vscode.StatusBarItem;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "atoll-extension" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand("atoll-extension.helloWorld", () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage("Hello World from Atoll Extension!");
+    });
+
+    context.subscriptions.push(disposable);
+
+    const myCommandId = "atoll-extension.showSelectionCount";
+    let disposable2 = vscode.commands.registerCommand(myCommandId, () => {
+        // const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+        // vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
+        vscode.window.showInformationMessage(
+            "gh-401: As a developer, I can use Atoll within VS Code, so that I can be more productive"
+        );
+    });
+    context.subscriptions.push(disposable2);
+
+    myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10000);
+    myStatusBarItem.command = myCommandId;
+    context.subscriptions.push(myStatusBarItem);
+
+    // register some listener that make sure the status bar
+    // item always up-to-date
+    // context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+    // context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+    // update status bar item once at start
+    updateStatusBarItem();
+}
+
+function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
+    let lines = 0;
+    if (editor) {
+        lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
+    }
+    return lines;
+}
+
+function updateStatusBarItem(): void {
+    // https://code.visualstudio.com/api/references/icons-in-labels
+    myStatusBarItem.text = `$(extensions-star-full) gh-401 » basic extension UI`;
+
+    myStatusBarItem.show();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
